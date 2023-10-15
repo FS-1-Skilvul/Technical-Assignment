643bca997002dd0f5a77f01216f189aaf82d437e
Syarif Muhammad Alhaiza <alqadrihaiza@gmail.com>
2023-10-15 20:28:14 +0700

    login register berfungsi baik

--- login-regis.js
+++ login-regis.js
@@ -12,7 +12,7 @@ function register(e) {
   };
 
   let json = JSON.stringify(user);
-  localStorage.setItem(namaLengkap, json);
+  localStorage.setItem("email", json);
 
   result.innerHTML = `Nama Akun Dengan Nama Lengkap : ${namaLengkap} Berhasil dibuat!`;
 }
@@ -21,13 +21,21 @@ function loginFunc(e) {
   event.preventDefault();
 
   let email = document.getElementById("email").value;
+  console.log(email);
   let password = document.getElementById("password").value;
+  console.log(password);
   let result = document.getElementById("result");
+  console.log(result);
 
-  let user = localStorage.getItem(email);
+  let user = localStorage.getItem("email");
+  console.log(user);
   let data = JSON.parse(user);
+  console.log(data);
 
-  if ((user = null)) {
+  console.log(data.email);
+  console.log(data.password);
+
+  if (user == null) {
     result.innerHTML = "Masukan Email dan Password dengan Benar!";
   } else if (email == data.email && password == data.password) {
     result.innerHTML = "BERHASIL LOGIN!";
