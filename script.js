9af26d75dfa6bb23dd24c40fea6c92f5e113913e
rivazahra <riva.zahra88@gmail.com>
2023-10-16 16:46:17 +0700

    perbaiki kategori

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// function setActive(element) {
+//   // Remove the "active" class from all navbar items
+//   const navbarItems = document.querySelectorAll('nav a');
+//   navbarItems.forEach(item => item.classList.remove('active'));
+
+//   // Add the "active" class to the clicked navbar item
+//   element.classList.add('active');
+// }
+
+const nav = document.querySelector("nav");
+
+nav.addEventListener("click", (e) => {
+    // Hanya mengubah warna jika yang diklik adalah elemen <a>
+    if (e.target.tagName === 'A') {
+        // Hapus kelas "active" dari semua elemen <a> di dalam <nav>
+        const navLinks = document.querySelectorAll("nav a");
+        navLinks.forEach(link => link.classList.remove('active'));
+
+        // Tambahkan kelas "active" pada elemen <a> yang diklik
+        e.target.classList.add('active');
+    }
+});
+
+
 async function getCourses() {
   try {
     const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
@@ -35,17 +59,49 @@ getCourses();
 
 
 
-async function filterkategori(){
-  let kategori = document.getElementById(category)
-    const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
-    let item = await apiUrl.json();
-    const filterData = item.filter((course) => course.category === kategori.value
-    )
+// async function filterkategori(){
+//   let kategori = document.getElementById(category)
+//     const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
+//     let item = await apiUrl.json();
+//     const filterData = item.filter((course) => course.category === kategori.value
+//     )
 
-    filterData.forEach((course) => {
-      let listCourse = document.getElementById("apiData");
-      let card = ``;
+//     filterData.forEach((course) => {
+//       let listCourse = document.getElementById("apiData");
+//       let card = ``;
 
       
-    })
-  }
+//     })
+//   }
+
+
+
+  // async function myapp() {
+  //   const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
+  //       let item = await apiUrl.json();
+  //   const buttons = document.querySelectorAll("li");
+  //   const cards = document.querySelectorAll(".card");
+  
+  //   function filter(category) {
+  //     item.forEach((item) => {
+  //       const isItemFiltered = !item.classList.contains(category);
+  //       const isShowAll = category.toLowerCase() === "semua kelas";
+  //       if (isItemFiltered && !isShowAll) {
+  //         item.classList.add("hide");
+  //       } else {
+  //         item.classList.remove("hide");
+  //       }
+  //     });
+  //   }
+  
+  //   buttons.forEach((button) => {
+  //     button.addEventListener("click", () => {
+  //       const currentCategory = button.dataset.filter;
+  //       console.log(currentCategory);
+  //       filter(currentCategory, cards);
+  //     });
+  //   });
+  // }
+  
+  // myapp();
+  
