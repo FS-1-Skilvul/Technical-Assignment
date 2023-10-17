6e711055bca246f70ecc35ce6163626288833896
rivazahra <riva.zahra88@gmail.com>
2023-10-17 16:31:09 +0700

    filter cari kelas by category

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-// function setActive(element) {
-//   // Remove the "active" class from all navbar items
-//   const navbarItems = document.querySelectorAll('nav a');
-//   navbarItems.forEach(item => item.classList.remove('active'));
-
-//   // Add the "active" class to the clicked navbar item
-//   element.classList.add('active');
-// }
-
 const nav = document.querySelector("nav");
 
 nav.addEventListener("click", (e) => {
-    // Hanya mengubah warna jika yang diklik adalah elemen <a>
-    if (e.target.tagName === 'A') {
-        // Hapus kelas "active" dari semua elemen <a> di dalam <nav>
-        const navLinks = document.querySelectorAll("nav a");
-        navLinks.forEach(link => link.classList.remove('active'));
+  // Hanya mengubah warna jika yang diklik adalah elemen <a>
+  if (e.target.tagName === "A") {
+    // Hapus kelas "active" dari semua elemen <a> di dalam <nav>
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach((link) => link.classList.remove("active"));
 
-        // Tambahkan kelas "active" pada elemen <a> yang diklik
-        e.target.classList.add('active');
-    }
+    // Tambahkan kelas "active" pada elemen <a> yang diklik
+    e.target.classList.add("active");
+  }
 });
-
 
 async function getCourses() {
   try {
     const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
-   
+
     let item = await apiUrl.json();
     const limit = 12;
     const sliceData = item.slice(0, limit);
@@ -33,7 +23,7 @@ async function getCourses() {
       let listCourse = document.getElementById("apiData");
       let card = `
    
-   <div class="col-category col-6 " style=" max-width: 17rem">
+   <div class="col-category col-6  mx-auto" style=" max-width: 17rem">
     <div class="card h-100 shadow-lg">
     <img src="${course.image}" class="card-img-top p-1 shadow-sm" alt="...">
     <div class="card-body d-flex flex-column justify-content-between">
@@ -57,51 +47,66 @@ async function getCourses() {
 }
 getCourses();
 
+// --------------------- filter
 
+async function filterAndDisplayCards() {
+  try {
+    // Fetch data from the API (replace with your actual API URL)
+    const apiUrl = "https://65280fcd931d71583df1d176.mockapi.io/course";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-// async function filterkategori(){
-//   let kategori = document.getElementById(category)
-//     const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
-//     let item = await apiUrl.json();
-//     const filterData = item.filter((course) => course.category === kategori.value
-//     )
+    // Get the category selected in the dropdown
+    const categorySelect = document.getElementById("category-filter");
+    const selectedCategory = categorySelect.value;
 
-//     filterData.forEach((course) => {
-//       let listCourse = document.getElementById("apiData");
-//       let card = ``;
+    // Filter cards based on the selected category from the dropdown
+    // const filteredCards = selectedCategory === "all" ? data : data.filter((course) => course.category.trim() === selectedCategory.trim());
 
-      
-//     })
+    const filteredCards = selectedCategory === "all" ? data : data.filter((course) => course.category === selectedCategory);
+    console.log(filteredCards);
+    // Display filtered cards in the container
+    const cardsContainer = document.getElementById("apiData");
+    cardsContainer.innerHTML = "";
+
+    filteredCards.forEach((course) => {
+      const card = document.createElement("div");
+      // card.className = "col-category col-6";
+      // card.style.maxWidth = "17rem";
+
+      card.innerHTML = `  
+   
+       <div class="col mx-auto" style=" max-width: 17rem">
+    <div class="card h-100 shadow-lg">
+    <img src="${course.image}" class="card-img-top p-1 shadow-sm" alt="...">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">${course.coursename}</h5>
+      <div class="card-text fw-bold
+       ">${course.category}</div>
+      <p class="card-text">${course.description}</p>
+      <div class="d-flex justify-content-around align-items-center">
+  <h2 class="" style="font-size: 18px">${course.price}</h2>
+<a href="#" class="btn btn-primary " style="color:#ffff">Ikuti Kelas</a>
+            </div>
+    </div>
+    </div>
+    </div>`;
+      cardsContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.getElementById("category-filter").addEventListener("change", filterAndDisplayCards);
+// function redirectToFilter() {
+//   try {
+//     // Get the selected category from the dropdown
+//     const selectedCategory = document.getElementById("category-filter").value;
+
+//     // Redirect to filter.html with the selected category as a query parameter
+//     window.location.href = `filter.html?category=${encodeURIComponent(selectedCategory)}`;
+//   } catch (error) {
+//     console.log(error);
 //   }
-
-
-
-  // async function myapp() {
-  //   const apiUrl = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
-  //       let item = await apiUrl.json();
-  //   const buttons = document.querySelectorAll("li");
-  //   const cards = document.querySelectorAll(".card");
-  
-  //   function filter(category) {
-  //     item.forEach((item) => {
-  //       const isItemFiltered = !item.classList.contains(category);
-  //       const isShowAll = category.toLowerCase() === "semua kelas";
-  //       if (isItemFiltered && !isShowAll) {
-  //         item.classList.add("hide");
-  //       } else {
-  //         item.classList.remove("hide");
-  //       }
-  //     });
-  //   }
-  
-  //   buttons.forEach((button) => {
-  //     button.addEventListener("click", () => {
-  //       const currentCategory = button.dataset.filter;
-  //       console.log(currentCategory);
-  //       filter(currentCategory, cards);
-  //     });
-  //   });
-  // }
-  
-  // myapp();
-  
+// }
