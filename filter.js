2ab832811d4166c7b523bdf6270e4dbaf7ad60d8
rivazahra <riva.zahra88@gmail.com>
2023-10-18 14:38:21 +0700

    filter category in home

--- filter.js
+++ filter.js
@@ -38,54 +38,109 @@
 //     console.error("Terjadi kesalahan dalam mengambil data dari API", error);
 //   });
 
-async function filterAndDisplayCards(category) {
-  try {
-    // Fetch data from the API (replace with your actual API URL)
-    const apiUrl = "https://65280fcd931d71583df1d176.mockapi.io/course";
-    const response = await fetch(apiUrl);
-    const data = await response.json();
 
-    // Filter cards based on the category
-    const filteredCards = data.filter((course) => course.category === category);
+document.addEventListener("DOMContentLoaded", function () {
+  const params = new URLSearchParams(window.location.search);
+  const category = params.get("category");
 
-    // Display filtered cards in the container
+  async function getData() {
+    try {
+      const response = await fetch("https://65280fcd931d71583df1d176.mockapi.io/course");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  getData().then((dataKelas) => {
     const cardsContainer = document.getElementById("apiFilter");
     cardsContainer.innerHTML = "";
 
+    
+    // Filter cards based on the specified category
+    const filteredCards = dataKelas.filter((course) => course.category === category);
+    
+        const judul = document.createElement("h1")
+        // judul.textContent = ${course.coursename}
+
     filteredCards.forEach((course) => {
+      `<h1 class="">${course.coursename}</h1>`
       let card = `
-        <div class="col-category col-6 " style="max-width: 17rem">
-          <div class="card h-100 shadow-lg">
-            <img src="${course.image}" class="card-img-top p-1 shadow-sm" alt="...">
-            <div class="card-body d-flex flex-column justify-content-between">
-              <h5 class="card-title">${course.coursename}</h5>
-              <div class="card-text fw-bold">${course.category}</div>
-              <p class="card-text">${course.description}</p>
-              <div class="d-flex justify-content-around align-items-center">
-                <h2 class="" style="font-size: 18px">${course.price}</h2>
-                <a href="#" class="btn btn-primary" style="color:#ffff">Ikuti Kelas</a>
-              </div>
-            </div>
-          </div>
-        </div>`;
+      
+                    <div class="col-category col-6 " style="max-width: 17rem">
+                        <div class="card h-100 shadow-lg">
+                            <img src="${course.image}" class="card-img-top p-1 shadow-sm" alt="...">
+                            <div class="card-body d-flex flex-column justify-content-between">
+                                <h5 class="card-title">${course.coursename}</h5>
+                                <div class="card-text fw-bold">${course.category}</div>
+                                <p class="card-text">${course.description}</p>
+                                <div class="d-flex justify-content-around align-items-center">
+                                    <h2 class="" style="font-size: 18px">${course.price}</h2>
+                                    <a href="#" class="btn btn-primary" style="color:#ffff">Ikuti Kelas</a>
+                                </div>
+                            </div>
+                        </div>
+                     
+                    </div>`;
       cardsContainer.innerHTML += card;
     });
-      
-       const newUrl = `filter.html?category=${category}`;
-       window.history.pushState({ category: category }, null, newUrl);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Event listener for category links
-document.querySelectorAll(".category-link").forEach((link) => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-    const selectedCategory = this.getAttribute("data-category");
-    filterAndDisplayCards(selectedCategory);
   });
 });
+
+
+// async function filterAndDisplayCards(category) {
+//   try {
+//     // Fetch data from the API (replace with your actual API URL)
+//     const apiUrl = "https://65280fcd931d71583df1d176.mockapi.io/course";
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+
+//     // Filter cards based on the category
+//     const filteredCards = data.filter((course) => course.category === category);
+
+//     // Display filtered cards in the container
+//     const cardsContainer = document.getElementById("apiFilter");
+//     cardsContainer.innerHTML = "";
+
+//     filteredCards.forEach((course) => {
+//       let card = `
+//         <div class="col-category col-6 " style="max-width: 17rem">
+//           <div class="card h-100 shadow-lg">
+//             <img src="${course.image}" class="card-img-top p-1 shadow-sm" alt="...">
+//             <div class="card-body d-flex flex-column justify-content-between">
+//               <h5 class="card-title">${course.coursename}</h5>
+//               <div class="card-text fw-bold">${course.category}</div>
+//               <p class="card-text">${course.description}</p>
+//               <div class="d-flex justify-content-around align-items-center">
+//                 <h2 class="" style="font-size: 18px">${course.price}</h2>
+//                 <a href="#" class="btn btn-primary" style="color:#ffff">Ikuti Kelas</a>
+//               </div>
+//             </div>
+//           </div>
+//         </div>`;
+//       cardsContainer.innerHTML += card;
+//     });
+      
+//        const newUrl = `filter.html?category=${category}`;
+//        window.history.pushState({ category: category }, null, newUrl);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// // Event listener for category links
+// document.querySelectorAll(".category-link").forEach((link) => {
+//   link.addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent the default behavior of the link
+//     const selectedCategory = this.getAttribute("data-category");
+//     filterAndDisplayCards(selectedCategory);
+//   });
+// });
 
 // async function filterAndDisplayCards() {
 //   try {
